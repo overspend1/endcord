e9ad681499f22b5c9f7abaeae855ff2538f7e30c
sparklost <marko.b.zivic@gmail.com>
2025-12-10 15:51:45 +0100

    Fix game detection crashing on macos

--- endcord/game_detection.py
+++ endcord/game_detection.py
@@ -4,6 +4,7 @@ import os
 import sys
 import threading
 import time
+import traceback
 from datetime import datetime
 
 try:
@@ -186,6 +187,7 @@ def get_user_processes_diff_darwin():
             continue
         if not cmdline:
             continue
+        cmdline = cmdline[0]
 
         # add to cache and newly added processes
         path = cmdline.replace("\\", "/").replace("\x00", "")
@@ -320,7 +322,11 @@ class GameDetection:
         del outdated
 
         # update last seen times in cache
-        added, _ = get_user_processes_diff()
+        try:
+            added, _ = get_user_processes_diff()
+        except BaseException as e:
+            logger.error(f"Game detection service stopped because of error:/n{"".join(traceback.format_exception(e))}")
+            return
         global proc_cache
         proc_cache = {}
         for proc_path in added:
@@ -333,7 +339,11 @@ class GameDetection:
         cache_changed = True   # to save updated times
         _get_user_processes_diff = get_user_processes_diff
         while self.run:
-            added, removed = _get_user_processes_diff()
+            try:
+                added, removed = _get_user_processes_diff()
+            except BaseException as e:
+                logger.error(f"Game detection service stopped because of error:/n{"".join(traceback.format_exception(e))}")
+                return
 
             for proc_path in added:
                 proc = cache.get(proc_path)
